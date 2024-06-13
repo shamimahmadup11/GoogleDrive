2d17d794f93fbfb1922e1807c3227d31a412e291
shamimahmadup11 <shamimahmadup1@gmail.com>
2024-06-13 15:59:59 +0530

    done

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -5,14 +5,13 @@ import "firebase/auth";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCvkJy1wJmsqsc6tG5VlXf-jLdwjwfnKUA",
-  authDomain: "driveproject-b3219.firebaseapp.com",
-  projectId: "driveproject-b3219",
-  storageBucket: "driveproject-b3219.appspot.com",
-  messagingSenderId: "130551430391",
-  appId: "1:130551430391:web:dbc77839b5a654216867f2"
+  apiKey: "AIzaSyBwvLNvqu6gcZ373lzE6ARxoP77fF_9Mz0",
+  authDomain: "drive-1c55e.firebaseapp.com",
+  projectId: "drive-1c55e",
+  storageBucket: "drive-1c55e.appspot.com",
+  messagingSenderId: "485959828898",
+  appId: "1:485959828898:web:5939b50b4607578d881efb"
 };
-
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
